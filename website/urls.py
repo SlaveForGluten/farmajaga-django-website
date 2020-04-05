0dbc0427522bb6a760cb3eea32210122b59d4fb0
Aleksander <alegzanderr@gmail.com>
2020-04-05 07:16:33 +0100

    testing displaying posts

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import NewsListPL  #NewsListEN
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home_pl, name='website-home/pl'),
+    path('news/pl/', NewsListPL.as_view(), name='website-news/pl'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 
-def index(request):
-    return render(request, 'website/home.html')
+def home_pl(request):
+    return render(request, 'website/pl/home.html')
 
+
+class NewsListPL(ListView):
+    model = Post
+    template_name = 'website/pl/news.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5

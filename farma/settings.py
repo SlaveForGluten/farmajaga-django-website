91aa2bdf40ff6f86055d408ccb1c39f725ff2d5b
Aleksander <alegzanderr@gmail.com>
2020-04-05 13:23:51 +0100

    deploying static files to S3

--- farma/settings.py
+++ farma/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-# STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'myproject.storage.S3Storage'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'

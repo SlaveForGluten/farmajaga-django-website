4792cebbc7f7e098858edc35f9e70685791d4fb5
Aleksander <alegzanderr@gmail.com>
2020-04-06 06:51:04 +0100

    all templates added, links conected, static files conected

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,30 @@
 from django.urls import path
-from .views import NewsListPL  #NewsListEN
+from .views import NewsListPL, NewsListEN
 from . import views
 
 urlpatterns = [
-    path('', views.home_pl, name='website-home/pl'),
-    path('news/pl/', NewsListPL.as_view(), name='website-news/pl'),
+    path('', views.home_pl, name='farma_jaga-home/pl'),
+    path('news/pl/', NewsListPL.as_view(), name='farma_jaga-news/pl'),
+    path('contact/pl/', views.contact_pl, name='farma_jaga-contact/pl'),
+    path('products/pl/', views.products_pl, name='farma_jaga-products/pl'),
+
+
+    path('en/', views.home_en, name='farma_jaga-home/en'),
+    path('news/en/', NewsListEN.as_view(), name='farma_jaga-news/en'),
+    path('contact/en/', views.contact_en, name='farma_jaga-contact/en'),
+    path('products/en/', views.products_en, name='farma_jaga-products/en'),
+
+
+
+    # path('de/', views.home_de, name='farma_jaga-home/de'),
+    # path('news/de/', views.news_de, name='farma_jaga-news/de'),
+    # path('contact/de/', views.contact_de, name='farma_jaga-contact/de'),
+    # path('products/de/', views.products_de, name='farma_jaga-products/de'),
+
+
+
+    # path('sp/', views.home_sp, name='farma_jaga-home/sp'),
+    # path('news/sp/', views.news_sp, name='farma_jaga-news/sp'),
+    # path('contact/sp/', views.contact_sp, name='farma_jaga-contact/sp'),
+    # path('products/sp/', views.products_sp, name='farma_jaga-products/sp'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,8 +3,9 @@ from django.views.generic import ListView
 from .models import Post
 
 
+# links to Polish version of the site-------------------------
 def home_pl(request):
-    return render(request, 'website/pl/home.html')
+    return render(request, 'website/pl/home.html', {'title': 'Strona domowa'})
 
 
 class NewsListPL(ListView):
@@ -13,3 +14,67 @@ class NewsListPL(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
+
+
+def contact_pl(request):
+    return render(request, 'website/pl/contact.html', {'title': 'Kontakt'})
+
+
+def products_pl(request):
+    return render(request, 'website/pl/products.html', {'title': 'Produkty'})
+
+
+# English------------------------------------------------------
+def home_en(request):
+    return render(request, 'website/en/home.html', {'title': 'Home'})
+
+
+class NewsListEN(ListView):
+    model = Post
+    template_name = 'website/en/news.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+
+def contact_en(request):
+    return render(request, 'website/en/contact.html', {'title': 'Contact'})
+
+
+def products_en(request):
+    return render(request, 'website/en/products.html', {'title': 'Products'})
+
+"""
+#German--------------------------------------------------
+def home_de(request):
+    return render(request, 'website/de/home.html', {'title': 'Home'})
+
+
+def news_de(request):
+    return render(request, 'website/de/news.html', {'title': 'News'})
+
+
+def contact_de(request):
+    return render(request, 'website/de/contact.html', {'title': 'Contact'})
+
+
+def products_de(request):
+    return render(request, 'website/de/products.html', {'title': 'Products'})
+
+
+#Spanish------------------------------------------------
+def home_sp(request):
+    return render(request, 'website/sp/home.html', {'title': 'Home'})
+
+
+def news_sp(request):
+    return render(request, 'website/sp/news.html', {'title': 'News'})
+
+
+def contact_sp(request):
+    return render(request, 'website/sp/contact.html', {'title': 'Contact'})
+
+
+def products_sp(request):
+    return render(request, 'website/sp/products.html', {'title': 'Products'})
+"""
